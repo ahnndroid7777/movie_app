79ad031a8b44d2b134f6b0f338b2dc1556969e29
Hyungjin An <super702@gmail.com>
2018-09-28 16:45:38 +0900

    React에서 제공하는 Life-cycle APIs 관련 내용 메모
    리액트에서 특정 타이밍에 자동으로 불리게 되는 Life-cycle APIs 관련 노트 => 렌더링 및 props 업데이트 타이밍 별 자동 호출 API들에 대해 노트 기입

--- src/App.js
+++ src/App.js
@@ -23,7 +23,42 @@ const movies = [
 ]
 
 class App extends Component {
+
+  /**
+   * [ Life-Cycle APIs 주요 내용 메모 ]
+   * 
+   * 1) Life-Cycle API란?
+   *    : React에 내장되어 특정 시점에 React에 의해 자동으로 불리게 되는 API
+   *    : 컴포넌트의 행위별 생명주기에 따라 자동으로 진행되며, 개발자는 해당 생명주기를 이해하게 되면 적정 시점에 Life-Cycle API를 이용해 원하는 로직이 구동되도록 개발 가능
+   * 
+   * 
+   * 2) 렌더링(Rendering) 관련 Life-Cycle APIs 실행 순서
+   *    : componentWillMount() -> render() -> componentDidMount()
+   * 
+   *    e.g. 영화앱을 만들 경우, componentWillMount()가 불리는 시점에 API 서버로부터 영화 관련 데이터를 얻어오고
+   *         render가 불리는 시점에 가져온 데이터를 이용해 화면을 렌더링 처리하도록 프로그래밍 가능
+   * 
+   * 
+   * 3) 업데이트(Update) 관련 Life-Cycle APIs 실행 순서
+   *    : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+   * 
+   *    STEP 1. componentWillReceiveProps() : 부모 컴포넌트로부터 props를 전달받기 위한 수신 대기
+   *    STEP 2. shouldComponentUpdate() : 만약 부모 컴포넌트로부터 전달된 props의 변경점이 존재하는지 판단
+   *    STEP 3. componentWillUpdate() : 변경점이 존재할 경우 업데이트 준비
+   *    STEP 4. render() : 변경분 데이터에 대한 렌더링 처리
+   *    STEP 5. componentDidUpdate() : props 변경분에 대한 업데이트 완료
+   */
+
+  componentWillMount() {
+    console.log('will mount.');    
+  }
+
+  componentDidMount() {
+    console.log('did mount.');    
+  }
+
   render() {
+    console.log('rendering...');
     return (
       <div className="App">
         {movies.map((movie, index) => {
